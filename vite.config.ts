6df41286b508a33e4fa8d67647e0409bc8974fa0
Usamah Muhammad <usamah0071@hotmail.com>
2026-05-02 15:03:57 +0100

    Fix vite allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        allowedHosts: ['fastautopass.co.uk', 'www.fastautopass.co.uk', 'all'],
       },
       plugins: [react()],
       define: {
